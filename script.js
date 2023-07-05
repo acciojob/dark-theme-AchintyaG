8aecc0094d41710176773bee208070b04ae348aa
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-07-05 21:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //your code here
-
+var theme = document.getElementById('#ptag');
+var swapBtn = document.querySelector(button);
+ function swapTheme() {
+ 	if()
+ }
+swapBtn.addEventListner('click', swapTheme);
